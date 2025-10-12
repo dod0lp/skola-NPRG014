c2538fa6b9d135991c2854f371c895235375bd3c
dod0lp <dodotv007@gmail.com>
2025-10-13 00:41:20 +0200

    Added code that seems to kinda work, but there is bug that createdAt method doesnt save createdAt, but when called getter

--- lecture1013/homework/Timestamp.groovy
+++ lecture1013/homework/Timestamp.groovy
@@ -9,18 +9,19 @@ import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.ast.ClassNode
 import org.codehaus.groovy.ast.Parameter
 import org.codehaus.groovy.control.SourceUnit
-import org.codehaus.groovy.transform.ASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformationClass
 import org.codehaus.groovy.ast.stmt.TryCatchStatement
 import static org.codehaus.groovy.control.CompilePhase.SEMANTIC_ANALYSIS
-import org.codehaus.groovy.ast.builder.AstBuilder
-import org.codehaus.groovy.ast.stmt.BlockStatement
-import org.codehaus.groovy.syntax.SyntaxException
-import org.codehaus.groovy.control.messages.SyntaxErrorMessage
-import groovyjarjarasm.asm.Opcodes
-import org.codehaus.groovy.ast.ClassHelper
-import static org.codehaus.groovy.ast.tools.GeneralUtils.*
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.ast.*
+import org.codehaus.groovy.ast.expr.*
+import org.codehaus.groovy.ast.stmt.*
+import org.codehaus.groovy.control.*
+import org.codehaus.groovy.syntax.*
+import org.codehaus.groovy.transform.*
+import static org.codehaus.groovy.syntax.Types.*
+
 
 @Retention(RetentionPolicy.SOURCE)
 @Target([ElementType.TYPE])
@@ -32,6 +33,9 @@ public @interface CreatedAt {
 
 @GroovyASTTransformation(phase = SEMANTIC_ANALYSIS)
 public class CreatedAtTransformation implements ASTTransformation {
+    static final int ACC_PRIVATE = 2
+    static final int ACC_PUBLIC = 1
+    static final int ACC_FINAL = 16
 
     public void visit(ASTNode[] astNodes, SourceUnit source) {
 
@@ -42,7 +46,7 @@ public class CreatedAtTransformation implements ASTTransformation {
         // Additionally, all existing methods of the class should be enhanced so that they reset the time stored in the field to the current time,
         // whenever they are called, but ONLY if more than 1 second has elapsed since the latest update to the time stored in the field.
         // A new method, named "clearTimestamp()" must be added to the class. This method sets the time stored in the field to "0".
-                
+
         // Fill in the missing AST generation code to make the script pass
         // You can take inspiration from exercises
         // Documentation and hints:
@@ -52,14 +56,93 @@ public class CreatedAtTransformation implements ASTTransformation {
         // http://docs.groovy-lang.org/docs/groovy-latest/html/api/org/codehaus/groovy/ast/stmt/package-summary.html
         // http://docs.groovy-lang.org/docs/groovy-latest/html/api/org/codehaus/groovy/ast/tools/package-summary.html        
         // http://docs.groovy-lang.org/docs/groovy-latest/html/api/org/codehaus/groovy/ast/tools/GeneralUtils.html
-        
+
         // Use ClassHelper.long_TYPE to specify a long type.
         // buildFromString() returns an array, which holds a BlockStatement for the passed-in code as its first element.
         // ClassNode.addField() accepts an expression, which can be obtained from a BlockStatement as blockStatement.statements.expression
         // ClassNode.addMethod() accepts a BlockStatement
-        
+
         //TODO Implement this method
-        
+        AnnotationNode annotationNode = astNodes[0]
+        if (!(astNodes[1] instanceof ClassNode)) return
+        ClassNode classNode = astNodes[1]
+
+        // --- Add private long field ---
+        FieldNode timestampField = new FieldNode("__createdAt",
+                ACC_PRIVATE,
+                ClassHelper.long_TYPE,
+                classNode,
+                new ConstantExpression(0L))
+        classNode.addField(timestampField)
+
+        // --- Add public final getter method ---
+        String getterName = "createdAt"
+        def nameAttr = annotationNode.getMember("name")
+        if (nameAttr instanceof ConstantExpression && nameAttr.value) {
+            getterName = nameAttr.value
+        }
+
+        BlockStatement getterBody = new BlockStatement()
+        getterBody.addStatement(new ReturnStatement(new VariableExpression("__createdAt")))
+
+        MethodNode getterMethod = new MethodNode(getterName,
+                ACC_PUBLIC | ACC_FINAL,
+                ClassHelper.long_TYPE,
+                Parameter.EMPTY_ARRAY,
+                ClassNode.EMPTY_ARRAY,
+                getterBody)
+        classNode.addMethod(getterMethod)
+
+        // --- "Enhance" all existing methods ---
+        classNode.methods.each { MethodNode method ->
+            // smth i seen in docs
+            if (method.isSynthetic() || method.name.contains("<init>")) return
+            BlockStatement originalCode = method.code instanceof BlockStatement ? method.code : new BlockStatement()
+
+            // update timestamp if more than 1s has passed
+            BlockStatement newCode = new BlockStatement()
+
+            // if (System.currentTimeMillis() - __createdAt > 1000) { __createdAt = System.currentTimeMillis() }
+            // --- if (System.currentTimeMillis() - this.__createdAt > 1000) { this.__createdAt = System.currentTimeMillis() } ---
+            newCode.addStatement(new IfStatement(
+                    // wrap the BinaryExpression in a BooleanExpression (required by IfStatement constructors)
+                    new BooleanExpression(new BinaryExpression(
+                            // left side: System.currentTimeMillis() - this.__createdAt
+                            new BinaryExpression(
+                                new MethodCallExpression(
+                                    new ClassExpression(ClassHelper.make(System)),
+                                        "currentTimeMillis",
+                                        ArgumentListExpression.EMPTY_ARGUMENTS),
+                                        Token.newSymbol(MINUS, -1, -1),
+                                        new FieldExpression(timestampField)),
+                            Token.newSymbol(COMPARE_GREATER_THAN, -1, -1),
+                            new ConstantExpression(1000L))),
+                    // then: this.__createdAt = System.currentTimeMillis()
+                    new ExpressionStatement(new BinaryExpression(new FieldExpression(timestampField),
+                            Token.newSymbol(EQUAL, -1, -1),
+                            new MethodCallExpression(new ClassExpression(ClassHelper.make(System)),
+                                    "currentTimeMillis",
+                                    ArgumentListExpression.EMPTY_ARGUMENTS))),
+                    EmptyStatement.INSTANCE))
+
+
+            newCode.addStatements(originalCode.statements)
+            method.code = newCode
+        }
+
+        // --- Add clearTimestamp() method ---
+        BlockStatement clearBody = new BlockStatement()
+        clearBody.addStatement(new ExpressionStatement(new BinaryExpression(new VariableExpression("__createdAt"),
+                Token.newSymbol(Types.EQUAL, -1, -1),
+                new ConstantExpression(0L))))
+
+        MethodNode clearMethod = new MethodNode("clearTimestamp",
+                ACC_PUBLIC,
+                ClassHelper.VOID_TYPE,
+                Parameter.EMPTY_ARRAY,
+                ClassNode.EMPTY_ARRAY,
+                clearBody)
+        classNode.addMethod(clearMethod)
     }
 }
 
