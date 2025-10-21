8a56c2d1b15062313700feb7b3b57ff61ef57ee0
dod0lp <dodotv007@gmail.com>
2025-10-21 14:50:47 +0200

    [HW4 - Scala] Solution done.

--- lecture-scala/exercises-homework/src/main/scala/h1/ComplexNumbers.scala
+++ lecture-scala/exercises-homework/src/main/scala/h1/ComplexNumbers.scala
@@ -3,13 +3,56 @@ import scala.language.implicitConversions
 
 // Add necessary class and object definitions in order to make the statements in the main work.
 
+
+case class Complex(re: Double, im: Double):
+	private def doubleToString(num: Double): String = {
+		val numStr = {
+			if num == num.toInt
+			then num.toInt.toString
+			else num.toString
+		}
+
+		numStr
+	}
+
+	override def toString: String = {
+		val reStr = doubleToString(re)
+		val imStr = doubleToString(im)
+
+		(im, re) match
+			case (0, _) => s"$reStr"
+			case (_, 0) => s"${imStr}i"
+			case (i, _)
+				if (i > 0) => s"$reStr+${imStr}i"
+			case (i, _)
+				if (i == 0) => s"$imStr"
+			case (i, _) => s"$reStr${imStr}i"
+	}
+
+	def +(other: Complex): Complex = {
+		Complex(re + other.re, im + other.im)
+	}
+
+	def *(other: Complex): Complex = {
+		Complex(re * other.re - im * other.im,
+						re * other.im + im * other.re)
+	}
+
+	def unary_- : Complex = Complex(-re, -im)
+
+
 object ComplexNumbers:
+	implicit def doubleToComplex(x: Double): Complex = Complex(x, 0)
+	object I extends Complex(0, 1)
+
 	def main(args: Array[String]): Unit =
-		/*
+
 		println(Complex(1,2)) // 1+2i
+
 
 		println(1 + 2*I + I*3 + 2) // 3+5i
 
 		val c = (2+3*I + 1 + 4*I) * I
 		println(-c) // 7-3i
-		*/
+
+    // println(Complex(0, 1)) // idk if this should be 0+1i or only 1i or perhaps only i
