b7bb1b83206e99dba7d2a9dbb82f45ecca0e3a70
dod0lp <dodotv007@gmail.com>
2025-10-29 23:17:01 +0100

    [HW2 Scala] Refactored to look like Scala. Added tests to check if valid results.

--- lecture-scala/exercises-homework/src/main/scala/h2/ExplicitStateTest.scala
+++ lecture-scala/exercises-homework/src/main/scala/h2/ExplicitStateTest.scala
@@ -44,11 +44,13 @@ class Thing extends WithExplicitState:
   protected var state = new ThingState("", (0, 0, 0))
 
   def setName(name: String): this.type = {
-    state = new ThingState(name, state.color); this
+    state = new ThingState(name, state.color)
+    this
   }
 
   def setColor(color: RGBColor): this.type = {
-    state = new ThingState(state.name, color); this
+    state = new ThingState(state.name, color)
+    this
   }
 
   override def toString: String = s"Thing ${state.name} with color ${state.color}"
@@ -70,28 +72,35 @@ trait History extends WithExplicitState:
 
 object ExplicitStateTest:
   def main(args: Array[String]): Unit =
+    val tests: Boolean = true
     // The inferred type of variable "john" should be "Person & History".
     val john = (new Person with History).setName("John Doe").setAge(24).checkpoint()
 
     println(john)
+    if (tests) println(john.toString.equals("Person John Doe aged 24"))
     john.setAge(25)
 
     println(john)
+    if (tests) println(john.toString.equals("Person John Doe aged 25"))
     println(john.history)
+    if (tests) println(john.history.toString.startsWith("List(h2.PersonState@"))
 
     val johnsPrevState = john.history(0)
     john.restoreTo(johnsPrevState)
     println(john)
+    if (tests) println(john.toString.equals("Person John Doe aged 24"))
 
     // The inferred type of variable "box" should be "Thing & History".
     val box = new Thing with History
     box.setName("Box")
     box.setColor((255, 0, 0))
     println(box)
+    if (tests) println(box.toString.equals("Thing Box with color (255,0,0)"))
 
     val joe = new Person with History
     joe.restoreTo(johnsPrevState).setName("Joe")
     println(joe)
+    if (tests) println(joe.toString.equals("Person Joe aged 24"))
 
-// The line below must not compile. It should complain about an incompatible type.
-// box.restoreTo(johnsPrevState)
+    // The line below must not compile. It should complain about an incompatible type.
+    // box.restoreTo(johnsPrevState)
