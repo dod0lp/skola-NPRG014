dd81c628d2e40fc46d37becf4938248d3a12f858
dod0lp <dodotv007@gmail.com>
2025-11-03 06:11:12 +0100

    [HW3 Scala] Almost done, first case fails.

--- lecture-scala/exercises-homework/src/main/scala/h3/Checker.scala
+++ lecture-scala/exercises-homework/src/main/scala/h3/Checker.scala
@@ -1,15 +1,10 @@
-/* Uncomment this to finalize the code
-
 package h3
 
 import scala.collection.mutable.ListBuffer
 
 abstract class Event
-
 case class Command(cmdName: String) extends Event
-
 case class Succeed(cmdName: String) extends Event
-
 case class Fail(cmdName: String) extends Event
 
 class Property(val name: String, val func: () => Boolean)
@@ -18,6 +13,7 @@ class Monitor[T]:
   val properties = ListBuffer.empty[Property]
 
   // def property /* Add declaration here */
+  def property(propName: String)(formula: => Boolean): Unit =
     properties += Property(propName, () => formula)
 
   var eventsToBeProcessed = List[T]()
@@ -25,19 +21,24 @@ class Monitor[T]:
   def check(events: List[T]) =
     for prop <- properties do
       eventsToBeProcessed = events
-
       val result = prop.func()
-
       println("Property \"" + prop.name + "\" ... " + (if (result) "OK" else "FAILED"))
-
 
   def require(func: PartialFunction[T, Boolean]): Boolean =
   /* Add body here
-   *
-   * to know whether a partial function is defined for a given event,
-   * use func.isDefinedAt(event).
-   */
-
+     *
+     * to know whether a partial function is defined for a given event,
+     * use func.isDefinedAt(event).
+     */
+    eventsToBeProcessed match
+      case Nil => true
+      case event :: rest =>
+        if func.isDefinedAt(event) then
+          val result = func(event)
+          eventsToBeProcessed = rest
+          result
+        else
+          true
 
 class MyMonitor extends Monitor[Event] :
   property("The first command should succeed or fail before it is received again") {
@@ -71,12 +72,17 @@ class MyMonitor extends Monitor[Event] :
   property("The first command should succeed") {
     /* Add a property definition here which requires that the first command does not fail.
      * It should yield OK with the events listed in the main method.
-     */
+    */
+    require {
+      case Command(c) =>
+        require {
+          case Fail(`c`) => false
+          case _ => true
+        }
+    }
   }
 
-
 object Checker:
-
   def main(args: Array[String]): Unit =
     val events = List(
       Command("take_picture"),
@@ -84,8 +90,5 @@ object Checker:
       Succeed("take_picture"),
       Fail("take_picture")
     )
-
     val monitor = new MyMonitor
     monitor.check(events)
-
- */
