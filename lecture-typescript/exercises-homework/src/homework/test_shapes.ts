c4230a152d53bab5579d6d37ff56271696d42535
dod0lp <dodotv007@gmail.com>
2025-12-15 16:30:13 +0100

    [10] Added comments for better compile/not-compile check.

--- lecture-typescript/exercises-homework/src/homework/test_shapes.ts
+++ lecture-typescript/exercises-homework/src/homework/test_shapes.ts
@@ -20,26 +20,26 @@ const shapes = [
     createSquare(10, {r: 255, g: 255, b: 0})
 ];
 
-// const t1 = createTriangle(10, 10, "yellow"); --- does not compile - "yellow" is not recognized
+// const t1 = createTriangle(10, 10, "yellow"); // --- does not compile - "yellow" is not recognized
 
 const r1 = createShape('rectangle', 10, 10, "red");
-// const r2: Circle = createShape('rectangle', 10, 10, "red"); --- does not compile, the result cannot be assigned to Circle
-// const r2 = createShape('rectangle', 10, "red"); --- does not compile, rectangle requires width and height
-// const r2 = createShape('rectangle', 10, 10, "yellow"); --- does not compile, "yellow" is not recognized
+// const r2: Circle = createShape('rectangle', 10, 10, "red"); // --- does not compile, the result cannot be assigned to Circle
+// const r2 = createShape('rectangle', 10, "red"); // --- does not compile, rectangle requires width and height
+// const r2 = createShape('rectangle', 10, 10, "yellow"); // --- does not compile, "yellow" is not recognized
 console.log(calculateShapeArea(r1));
 
 const s1 = createSquare(10, "blue");
-// console.log(calculateShapeArea(s1)); --- does not compile - s1 is not accepted
+// console.log(calculateShapeArea(s1)); // --- does not compile - s1 is not accepted
 
 
 const {r: blueR, g: blueG, b: blueB} = colorTable["blue"];
-// const {z} = colorTable["blue"]; --- does not compile - "z" is not recognized
-// const {r: yellowR, g: yellowG, b: yellowB}: NumericColor = colorTable["yellow"]; --- does not compile - "yellow" is not recognized
+// const {z} = colorTable["blue"]; // --- does not compile - "z" is not recognized
+// const {r: yellowR, g: yellowG, b: yellowB}: NumericColor = colorTable["yellow"]; // --- does not compile - "yellow" is not recognized
 
 const is1 = invertColor(s1);
 console.log(`is1 color: rgb(${is1.color.r}, ${is1.color.g}, ${is1.color.b})`);
 const is2: Square = is1;
-// const ic1: Circle = is1; --- does not compile, is1 cannot be assigned to Circle
+// const ic1: Circle = is1; // --- does not compile, is1 cannot be assigned to Circle
 
 
 const os1 = makeObservable(s1);
@@ -63,7 +63,7 @@ or1.on('color', (oldValue, newValue) => { // oldValue and newValue are typed as 
 })
 
 os1.update({ width: 15 });
-// os1.update({ height: 15 }); --- does not compile "height" is not recognized
+// os1.update({ height: 15 }); // --- does not compile "height" is not recognized
 or1.update({ height: 20, color: {r: 100, g: 100, b: 100} });
 
 
